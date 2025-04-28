bd345fdf980f3a80f128ee96319e04b8377c9f46
Matthew Sundquist <matthew.sundquist@usask.ca>
2025-04-28 00:31:39 -0600

    Finished function inside gates.c which defines behavior for AND, OR, XOR, and NOT gates. Updated boolheader.h to carry definitons for future nodes that will be used in the B-tree to link gates together

--- boolheader.h
+++ boolheader.h
@@ -1,3 +1,30 @@
 /* Created by Matthew Sundquist */
 
 
+#ifndef GATES_H
+#define GATES_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum {
+	AND_GATE,
+	OR_GATE,
+	XOR_GATE,
+	NOT_GATE,
+} GateType;
+
+int function_gate(GateType gate, int count, int *args);
+
+typedef struct internalNode {
+	struct internaNode *left, *right, *parent;
+	int (*function_gate)(int count, int *args);
+} internalNode;
+
+typedef struct leafNode {
+	int data;
+	struct internalNode *parent;
+} leafNode;
+
+#endif
---
+++ gates.c
@@ -0,0 +1,73 @@
+/* Created by Matthew Sundquist */
+/* Contains Definitions for all Gate Types Currently Supported */
+
+/* 
+
+	Returns:
+	0 -> off or low
+	1 -> on or high
+   -1 -> error
+
+*/
+
+#include <boolheader.h>
+
+int function_gate(GateType type, int count, int *args)
+{
+	if (args == null)
+		return -1;
+
+	switch (type)
+	{
+		case AND_GATE:
+			for (int i = 0; i < count, i++)
+			{
+				if (args[i] == 0)
+					return 0;
+			}
+
+			return 1;
+			break;
+
+		case OR_GATE:
+			for (int i = 0; i < count, i++)
+			{
+				if (args[i] == 1)
+					return 1;
+			}
+
+			return 0;
+			break;
+
+		case XOR_GATE:
+			int result = 0;
+			for (int i = 0; i < count, i++)
+			{
+				result ^= args[i];
+			}
+
+			return result;
+			break;
+
+			
+		case NOT_GATE:
+			if (count != 1)
+				return -1;
+
+			if (args[0] >= 1)
+				return 0;
+
+			else if (args[0] == 0)
+				return 1;
+
+			break;
+
+		default:
+			break;
+	}
+
+	
+	return -1;
+	
+	
+}	
