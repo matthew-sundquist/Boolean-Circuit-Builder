098f2bff0806df7a2e37983d2f85f745c1be13f5
Matthew Sundquist <matthew.sundquist@usask.ca>
2025-04-28 23:45:00 -0600

    updated gates_test to use a better-formatted testing approach using looping through functions instead of calling individual statements within the main() function

--- gates_test.c
+++ gates_test.c
@@ -1,19 +1,45 @@
 /* Testing for the gates.c file */
 
 #include <boolheader.h>
+#define NUM_TESTS 1
+
+
+int test_AND()
+{
+
+	int result = 0;
+	
+	int arr[2] = {1, 1};
+	if (function_gate(AND_GATE, 2, arr) != 1)
+	{
+		result++;
+	}
+
+	return result;
+}
+
+
+typedef int (*TestDriver)();
+
+TestDriver tests[] = {
+	test_AND
+};
+
 
 int main()
 {
 	
-	internalNode newNode;
-	internalNode *testNode = &newNode;
-	testNode->gate = AND_GATE;
-	
-	int arr[2] = {1, 1};
-	
-	printf("%i", function_gate(AND_GATE, 2, arr));
-		
+	int failed = 0;
 
+	for (int i = 0; i < NUM_TESTS; i++)
+	{
+		int result = tests[i]();
+		if (result != 0)
+		{
+			printf("Test %d failed with code %d\n", i + 1, result);
+			failed++;
+		}
+	}
 
-	return 0;
+	return failed;
 }
