52e3159ec3ec65dd3b7a54627618e917a7956ae8
Matthew Sundquist <matthew.sundquist@usask.ca>
2025-06-12 23:09:36 -0600

    created a depth first traversal function that goes to bottom of the tree and evaluates the boolean circuit from the leaf nodes which are inputs to the root node which is the last logic gate

--- tree_exec.c
+++ tree_exec.c
@@ -1,4 +1,48 @@
 /* Created by Matthew Sundquist */
 /* Tree Operations */
 
-int 
+#include <boolheader.h>
+
+/* recursivly traverses the tree and returns either a 1 or 0 */
+int depth_first_recursive(void *node, NodeType type)
+{
+
+	if (type == INTERNAL)
+	{
+		/* internal node */
+		/* recurse */
+			
+		internalNode *tmp = (internalNode *)(node);
+		int values[tmp->count];
+
+		for (int i = 0; i < tmp->count; i++)
+		{
+			results[i] = depth_first_recursive(tmp->nodes[i], tmp->types[i]);
+		}
+	}
+
+	else
+	{
+		/* leaf node */
+		/* do not recurse */
+
+		leafNode *tmp = (leafNode *)(root->nodes[i]);
+
+		return tmp->data;
+	}
+
+	
+	internalNode *gate = (internalNode *)(node);
+
+	return function_gate(internalNode->gate, gate->count, results);
+}
+
+int depth_first_tree(tree *tr)
+{
+	return depth_first_recursive(tr->root, INTERNAL);
+}
+
+int depth_first_node(internalNode *node)
+{
+	return depth_first_recursive(node, INTERNAL);
+}
